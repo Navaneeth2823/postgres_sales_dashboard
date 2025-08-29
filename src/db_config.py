10ae8da316df8a8cceb1702558f693db4e80b66f
Navaneeth Kumar K <navaneethkumark23@gmail.com>
2025-08-29 16:41:25 +0530

    Update db_config.py

--- src/db_config.py
+++ src/db_config.py
@@ -1,3 +1,3 @@
 # PostgreSQL DB connection URI for SQLAlchemy
 # Replace username, password, host, port and dbname as needed.
-DB_URI = "postgresql+psycopg2://postgres:Putta%40283@localhost:5432/sales_db"
+DB_URI = "postgresql+psycopg2://postgres:Yoyr_password@localhost:5432/sales_db"
